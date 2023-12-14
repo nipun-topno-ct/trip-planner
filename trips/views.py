8227aaa4f0738d0470549ea1bea99b5f181eb39d
nipun-topno-ct <nipun.topno@cleartrip.com>
2023-12-14 22:12:43 +0530

    Updated trip api to POST

--- trips/views.py
+++ trips/views.py
@@ -5,15 +5,24 @@ import openai
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
+@api_view(['POST'])
 def plan_trip(request):
     # Extract parameters from the request (e.g., location, time remaining)
-    location = request.GET.get('location')
-    time_remaining = request.GET.get('time_remaining')
-    places=request.GET.get('places')
+    request_body = json.loads(request.body)
+
+    location = request_body['location']
+    places = request_body['places']
 
     # GPT-3 prompt based on the user's input
-    prompt = f"im at {location} ,I want to explore {places},now give me a detailed plan for a trip accordingly such that I have {time_remaining} remaining and give me an order in which I have to visit them, include from,to, fare to explore ,journey_time,journey_cost,time to explore, give me the order also fare to explore give me the output only  in stringifield json format"
+    fields = [
+        '1. from: initial place',
+        '2. to: destination place',
+        '3. fare_to_explore: average money spent to explore the destination place',
+        '4. time_to_explore: how much time to be spent to explore the destination place (with time unit)',
+        '5. journey_time: time taken to travel from initial place to destination place (with time unit)',
+        '6. journey_cost: money spent to travel from initial place to destination place',
+    ]
+    prompt = f"im at {location} ,I want to explore {places},now give me a detailed plan for a trip accordingly and give me an order in which I have to visit them, give me the output only in stringified json format:" + " [ { required fields }, { required_fields}, ... ] where the required fields are: %s" % ', '.join(fields)
     print(prompt)
     print("????????\n\n")
 
